2ef9a347995bbf1bc8fa15fedd120b7b9d55b867
Matt Magdadaro <11400071@usc.edu.ph>
2023-09-19 20:31:24 +0800

    added useEffect functionality

--- SECOND/src/Todo.jsx
+++ SECOND/src/Todo.jsx
@@ -1,12 +1,17 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Delete from './Delete';
 
 function Todo() {
     const [todos, setTodos] = useState([]);
     const [newTodo, setNewTodo] = useState("");
+    const [disableAdd, setDisableAdd] = useState(false);
+
+    useEffect(() => {
+        setDisableAdd((newTodo === "" || !/^[a-zA-z\s]+$/.test(newTodo)))
+    }, [newTodo]);
 
     const handleAddTodo = () => {
-        if (newTodo) {
+        if (newTodo && /^[a-zA-z\s]+$/.test(newTodo)) {
             setTodos([...todos, newTodo]);
             setNewTodo("");
         }
@@ -20,10 +25,10 @@ function Todo() {
         <div>
             <h1>TODO LIST</h1>
             <input type="text" value={newTodo} onChange={(e) => setNewTodo(e.target.value)} />
-            <button onClick={handleAddTodo} style={{margin: '1rem'}}>Add</button>
+            <button onClick={handleAddTodo} style={{ margin: '1rem' }} disabled={disableAdd}>Add</button>
             <ul>
                 {todos.map
-                    ((todo, index) => (<li key={index}>{todo}<Delete index={index} onDelete={handleDeleteTodo}/></li>))}
+                    ((todo, index) => (<li key={index}>{todo}<Delete index={index} onDelete={handleDeleteTodo} /></li>))}
             </ul>
         </div>
     );
